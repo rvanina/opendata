94c9698ee03d5e853b72da303834921f084d1ad9
Alexander Khoroshikh <xad71rus@gmail.com>
2018-10-26 23:44:58 +0700

    feat(frontend): chart rendering for 1st levels of data

--- front/src/Chart/index.js
+++ front/src/Chart/index.js
@@ -2,25 +2,30 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import {Doughnut} from 'react-chartjs-2';
 
+import { fetchRegionalData } from '../api/methods'
+
 const Wrapper = styled.section`
     display: flex;
     padding: 0 20px;
 `;
 
-const testdata = {
-    datasets: [{
-        data: [10, 20, 30]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-        'Red',
-        'Yellow',
-        'Blue'
-    ]
-};
-
 export default class Chart extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: {},
+        }        
+    }
+
+    componentDidMount() {
+        fetchRegionalData().then(fetchedData => {
+            let labels = fetchedData.map(item => item.name)
+            let values = fetchedData.map(item => item.value)
+            let datasets = [{data: values}]
+            let data = { datasets, labels}
+            this.setState({data})
+        }).catch(error => console.log(error))
+    }
     render() {
         return (
             <Wrapper>
@@ -28,7 +33,7 @@ export default class Chart extends Component {
                     width={500} 
                     height={500} 
                     options={{ maintainAspectRatio: false }} 
-                    data={testdata} 
+                    data={this.state.data} 
                 />
             </Wrapper>
         )
