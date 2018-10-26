655cb40f8547735d9207249bd1de2b77530a9187
Alexander Khoroshikh <xad71rus@gmail.com>
2018-10-26 18:08:11 +0700

    feat(frontend): example layout

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
-import Helmet from 'react-helmet'
+import Helmet from 'react-helmet';
+import styled from 'styled-components';
 
 import Header from './Header';
+import Chart from './Chart';
+import Sidebar from './Sidebar';
+
+const ContentWrapper = styled.main`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+`;
 
 class App extends Component {
   render() {
@@ -11,6 +20,10 @@ class App extends Component {
           <title>Бюджет Томска | Открытые данные 2018</title>
         </Helmet>
         <Header/>
+        <ContentWrapper>
+          <Sidebar/>
+          <Chart/>
+        </ContentWrapper>
       </React.Fragment>
     );
   }
