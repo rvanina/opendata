4f0a2ed0034d0311f20019d802b94f07ead1bced
Alexander Khoroshikh <xad71rus@gmail.com>
2018-10-26 23:45:48 +0700

    fix(frontend): sidebar component cleared from trash

--- front/src/Sidebar/index.js
+++ front/src/Sidebar/index.js
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-import { fetchRegionalData } from '../api/methods'
-
 const Wrapper = styled.aside`
     display: flex;
     padding: 20px;
+    flex-flow: row wrap;
 `;
 
 export default class Sidebar extends Component {
-    state = {
-        data: []
-    }
-
-    componentWillMount() {
-        fetchRegionalData().then(data => this.setState({data})).catch(error => console.log(error))
-    }
-
     render() {
         return (
             <Wrapper>
                 Sidebar
-                {JSON.stringify(this.state.data)}
             </Wrapper>
         )
     }
