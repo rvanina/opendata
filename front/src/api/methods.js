9cdaee7ada43aa9dd9daf7877a1fcd0e52c4adc6
Alexander Khoroshikh <xad71rus@gmail.com>
2018-10-26 23:56:32 +0700

    feat(frontend): new fetchData method, takes (type, categoryId), type is regional or municipal

--- front/src/api/methods.js
+++ front/src/api/methods.js
@@ -1,6 +1,20 @@
 import axios from 'axios'
 import { HOSTNAME, PORT } from './settings'
 
+export const fetchData = (type, categoryId) => {
+    if (categoryId) {
+        return axios.get(`http://${HOSTNAME}:${PORT}/api/${type}?categoryId=${categoryId}`)
+        .then((response) => {
+            return response.data
+        })
+    } else {
+        return axios.get(`http://${HOSTNAME}:${PORT}/api/${type}`)
+        .then((response) => {
+            return response.data
+        })
+    }
+}
+
 export const fetchRegionalData = (categoryId) => {
     if (categoryId) {
         return axios.get(`http://${HOSTNAME}:${PORT}/api/regional?categoryId=${categoryId}`)
