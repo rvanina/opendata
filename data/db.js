ea3d1ab6166ea5286a438ef8c66c917375158cac
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2018-10-26 19:29:42 +0700

    feat: add data inserting

--- data/db.js
+++ data/db.js
@@ -19,11 +19,28 @@ const createTable = (name) => {
         value number
     )`
     
-    this.db.all(sql, [], (err, rows) => {
+    this.db.run(sql, [], (err, rows) => {
         if (err) {
             throw err
         }
         console.log(`Table ${name} was created.`)
+    })
+}
+
+const insertData = (data, tableName) => {
+    let sql = `INSERT INTO ${tableName}(name text, parent_id number,
+        value number)
+        VALUES(${data.name}, ${data.parent_id}, ${data.value})`
+
+    this.db.run(sql)
+}
+
+Data.prototype.query = (sql, parameters) => {
+    return this.db.all(sql, parameters, (err, row) => {
+        if (err) {
+            throw err
+        }
+        return row
     })
 }
 
