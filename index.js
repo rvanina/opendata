70e331f8c9192ee85aa2662553d2e270037f9e7a
Vladislav Kobyakov <ayakudere@gmail.com>
2018-10-27 12:28:07 +0700

    csv parsing

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express')
 
 const Api = require('./api')
-
+const CsvLoader = require('./data/csvLoader')
 const Data = require('./data/db')
-
-const db = new Data()
 
 const app = express()
 
-const port = 8008;
+const port = 80
 
 app.use('/', express.static('front/build'))
+
+const data = new Data()
 
 const api = new Api({
     data: null,
     db: db,
 })
+
+const csvLoader = new CsvLoader(data)
 
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', '*')
@@ -28,6 +30,18 @@ app.get('/api/regional', (req, res) => {
 
 app.get('/api/municipal', (req, res) => {
     res.json(api.getMunicipal(req.query.categoryId))
+})
+
+app.get('/parseData/:path', (req, res) => {
+    csvLoader
+        .loadData('municipal', req.params.path)
+        .then((data) => res.send(data))
+})
+
+app.get('/clearTable/:tableName', (req, res) => {
+    data
+        .clearTable(req.params.tableName)
+        .then(() => res.send('done'))
 })
 
 app.listen(port, function () {
