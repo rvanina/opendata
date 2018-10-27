ea2dea6405be64d062c9d9b2231fed94f149f6ad
Alexander Khoroshikh <xad71rus@gmail.com>
2018-10-27 10:20:56 +0700

    feat(frontend): sidebar button styling

--- front/src/Sidebar/index.js
+++ front/src/Sidebar/index.js
@@ -7,6 +7,22 @@ const Wrapper = styled.aside`
     flex-flow: column wrap;
 `;
 
+const Button = styled.button`
+    display: inline-block;
+    padding: 8px;
+    min-width: 300px;
+    background: #42BA78;
+    color: #ffffff;
+    border: none;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 20px;
+    font-size: 18px;
+    letter-spacing: 0.75px;
+    text-transform: uppercase; 
+`;
+
 export default class Sidebar extends Component {
     state = {
         currentType: 'regional',
@@ -23,8 +39,13 @@ export default class Sidebar extends Component {
     render() {
         return (
             <Wrapper>
-                Sidebar
-                <button onClick={this.handleChangeType}>{this.state.currentType}</button>
+                <Button onClick={this.handleChangeType}>
+                    {(this.state.currentType === 'regional') ? (
+                        'Томская область'
+                     ) : (
+                        'Томск'
+                     )}
+                </Button>
             </Wrapper>
         )
     }
