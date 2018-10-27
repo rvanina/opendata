2d3a6458a5757f7d82f0f4f3954380ad5f72284a
Alexander Khoroshikh <xad71rus@gmail.com>
2018-10-27 11:04:26 +0700

    feat(frontend): now can going down on click

--- front/src/Chart/index.js
+++ front/src/Chart/index.js
@@ -9,20 +9,30 @@ var randomColor = require('randomcolor');
 
 const Wrapper = styled.section`
     display: flex;
+    flex-flow: column nowrap;
     padding: 20px 20px 0 20px;
+`;
+
+const Title = styled.h2`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 16px;
+    font-size: 14px;
+    letter-spacing: 0.75px;
+    text-transform: uppercase; 
+    width: 300px;
 `;
 
 const legendOpts = {
     display: false,
 };
 
-let opts = {
+const opts = {
     maintainAspectRatio: false,
-    title: {
-        display: true,
-        text: 'Общий бюджет'
-    }
 }
+
+let chartTitle = '';
   
 
 export default class Chart extends Component {
@@ -31,20 +41,41 @@ export default class Chart extends Component {
         this.state = {
             type: this.props.type,
             data: {},
-        }        
+        }
+        this.handleClick = this.handleClick.bind(this);        
+    }
+
+    handleClick(elem) {
+        if (elem[0] !== undefined) {
+            let id = elem[0]._model.label.charAt(0)
+            let label = elem[0]._model.label.substring(1)
+            fetchData(this.state.type, id).then(fetchedData => {
+                chartTitle = label
+                let labels = fetchedData.map(item => item.id + item.name)
+                let values = fetchedData.map(item => item.value)
+                let backgroundColors = fetchedData.map(() => randomColor())
+                let ids = fetchedData.map(item => item.id)
+                let datasets = [{data: values, backgroundColor: backgroundColors, id: ids}]
+                let data = { datasets, labels}
+                this.setState({data})
+            }).catch(error => console.log(error))
+        } else {
+            console.log('chart background')
+        }
     }
 
     componentDidMount() {
         fetchData(this.state.type).then(fetchedData => {
             if (this.state.type === 'regional') {
-                opts.title.text = 'Общий бюджет Томской области'
+                chartTitle = 'Общий бюджет Томской области'
             } else {
-                opts.title.text = 'Общий бюджет Томска'
+                chartTitle = 'Общий бюджет Томска'
             }
-            let labels = fetchedData.map(item => item.name)
+            let labels = fetchedData.map(item => item.id + item.name)
             let values = fetchedData.map(item => item.value)
             let backgroundColors = fetchedData.map(() => randomColor())
-            let datasets = [{data: values, backgroundColor: backgroundColors}]
+            let ids = fetchedData.map(item => item.id)
+            let datasets = [{data: values, backgroundColor: backgroundColors, id: ids}]
             let data = { datasets, labels}
             this.setState({data})
         }).catch(error => console.log(error))
@@ -54,9 +85,9 @@ export default class Chart extends Component {
         if (this.props.type !== prevProps.type) {
           fetchData(this.props.type).then(fetchedData => {
             if (this.props.type === 'regional') {
-                opts.title.text = 'Общий бюджет Томской области'
+                chartTitle = 'Общий бюджет Томской области'
             } else {
-                opts.title.text = 'Общий бюджет Томска'
+                chartTitle = 'Общий бюджет Томска'
             }
             let labels = fetchedData.map(item => item.name)
             let values = fetchedData.map(item => item.value)
@@ -71,12 +102,13 @@ export default class Chart extends Component {
     render() {
         return (
             <Wrapper>
+                <Title>{chartTitle}</Title>
                 <Polar 
                     width={500} 
                     height={500} 
                     options={opts} 
                     data={this.state.data}
-                    getElementAtEvent={elem => console.log(elem)} 
+                    getElementAtEvent={this.handleClick} 
                     legend={legendOpts}
                 />
             </Wrapper>
