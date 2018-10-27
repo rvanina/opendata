bd652fcf40d38bb5747d6faeb3000a6823cc517c
Alexander Khoroshikh <xad71rus@gmail.com>
2018-10-27 14:12:42 +0700

    chore(frontend): deleted unused api methods

--- front/src/api/methods.js
+++ front/src/api/methods.js
@@ -15,31 +15,3 @@ export const fetchData = (type, categoryId) => {
     }
 }
 
-export const fetchRegionalData = (categoryId) => {
-    if (categoryId) {
-        return axios.get(`http://${HOSTNAME}:${PORT}/api/regional?categoryId=${categoryId}`)
-        .then((response) => {
-            return response.data
-        })
-    } else {
-        return axios.get(`http://${HOSTNAME}:${PORT}/api/regional`)
-        .then((response) => {
-            return response.data
-        })
-    }
-}
-
-export const fetchMunicipalData = (categoryId) => {
-    if (categoryId) {
-        return axios.get(`http://${HOSTNAME}:${PORT}/api/municipal?categoryId=${categoryId}`)
-        .then((response) => {
-            return response.data
-        })
-    } else {
-        return axios.get(`http://${HOSTNAME}:${PORT}/api/municipal`)
-        .then((response) => {
-            return response.data
-        })
-    }
-}
-
