967cfdbdeb6884a77360c40b1288ca6dee724d30
aleksandrhorosih <xad71rus@gmail.com>
2018-10-31 12:03:38 +0700

    feat(frontend): positioning of elems, responisivity

--- front/src/App.js
+++ front/src/App.js
@@ -8,8 +8,8 @@ import Sidebar from './Sidebar';
 
 const ContentWrapper = styled.main`
   display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-around;
+  flex-flow: row wrap;
+  justify-content: space-between;
 `;
 
 class App extends Component {
@@ -32,10 +32,12 @@ class App extends Component {
           <title>Бюджет Томска | Открытые данные 2018</title>
         </Helmet>
         <Header/>
-        <ContentWrapper>
-          <Sidebar setDataType={this.setDataType} data={this.state.value} />
-          <Chart type={this.state.type} updateData={this.updateData}/>
-        </ContentWrapper>
+        <div className="container">
+          <ContentWrapper>
+            <Sidebar setDataType={this.setDataType} data={this.state.value} />
+            <Chart type={this.state.type} updateData={this.updateData}/>
+          </ContentWrapper>
+        </div>
       </React.Fragment>
     );
   }
