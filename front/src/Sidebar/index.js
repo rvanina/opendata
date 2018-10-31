72c2368756832e1eaffea596159701257f3604c2
aleksandrhorosih <xad71rus@gmail.com>
2018-10-31 11:55:22 +0700

    feat(frontend): button in sidebar component

--- front/src/Sidebar/index.js
+++ front/src/Sidebar/index.js
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 
 const Wrapper = styled.aside`
     display: flex;
-    padding: 20px;
+    padding: 8px;
     flex-flow: column wrap;
+    border: 8px solid #42BA78;
+    margin-top: 12px;
 `;
 
 const Button = styled.button`
@@ -23,28 +25,35 @@ const Button = styled.button`
     text-transform: uppercase;
     cursor: pointer;
     &:hover {
-        background: #42BA56;
+        background: black;
     } 
 `;
 
 const Numbers = styled.div`
-    display: inline-block;
-    padding: 8px;
+    display: flex;
+    flex-flow: column nowrap;
+    padding: 8px 8px 16px 8px;
     max-width: 300px;
     background: none;
     border: none;
+    border-bottom: 4px solid #42BA78;
     font-family: Roboto;
     font-style: normal;
     font-weight: 400;
     line-height: 20px;
     font-size: 18px;
     letter-spacing: 0.75px;
-    & span {
-        line-height: 32px;
-        font-size: 28px;
-        color: #42BA78;
-        text-transform: uppercase;
-    } 
+    margin: 0 0 16px 0;
+    &:first-child {
+        padding-top: 16px;
+    }
+`;
+
+const Cost = styled.span`
+    line-height: 32px;
+    font-size: 28px;
+    color: #42BA78;
+    text-transform: uppercase;
 `;
 
 const TOMSK_POP = 595.2;
@@ -80,12 +89,15 @@ export default class Sidebar extends Component {
                 </Button>
                 <br/>
                 <Numbers>
+                    <Cost>{Math.round(val)} тыс.руб</Cost>
+                    Всего
+                </Numbers>
+                <Numbers>
                     {(this.state.currentType === 'regional') ? (
-                        <span>{Math.round((val/REG_POP)*100/100)} тыс.руб</span>
+                        <Cost>{Math.round((val/REG_POP)*100/100)} тыс.руб</Cost>
                         ) : (
-                        <span>{Math.round((val/TOMSK_POP)*100/100)} тыс.руб</span>
+                        <Cost>{Math.round((val/TOMSK_POP)*100/100)} тыс.руб</Cost>
                     )}
-                    <br/> 
                     {(this.state.currentType === 'regional') ? (
                         'На одного жителя Томской области'
                         ) : (
