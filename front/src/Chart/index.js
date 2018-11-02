2fcc3fd527267f145e06afc3ff5609da125b1fb5
aleksandrhorosih <xad71rus@gmail.com>
2018-11-02 15:49:04 +0700

    feat(Chart): tooltips now has shadow

--- front/src/Chart/index.js
+++ front/src/Chart/index.js
@@ -130,6 +130,7 @@ const opts = {
                     innerHtml += '<span>' + title + '</span>';
                 });
                 let style = 'background: #000000;';
+                style += 'box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.2), 0px 1px 18px rgba(0, 0, 0, 0.12), 0px 6px 10px rgba(0, 0, 0, 0.14);';
                 style += 'border: 1px solid #000000;';
                 style += 'border-radius: 4px;';
                 style += 'display: flex;';
