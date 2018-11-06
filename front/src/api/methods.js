32e0c1df4870f655a9846e95a858eaa4c8cc685d
aleksandrhorosih <xad71rus@gmail.com>
2018-11-06 13:06:06 +0700

    fix(frontend/api): cs according to airbnb

--- front/src/api/methods.js
+++ front/src/api/methods.js
@@ -1,17 +1,6 @@
-import axios from 'axios'
-import { HOSTNAME, PORT } from './settings'
+import axios from 'axios';
+import { HOSTNAME, PORT } from './settings';
 
-export const fetchData = (type, categoryId) => {
-    if (categoryId) {
-        return axios.get(`http://${HOSTNAME}:${PORT}/api/${type}?categoryId=${categoryId}`)
-        .then((response) => {
-            return response.data
-        })
-    } else {
-        return axios.get(`http://${HOSTNAME}:${PORT}/api/${type}`)
-        .then((response) => {
-            return response.data
-        })
-    }
-}
-
+export const fetchData = (type, categoryId) => axios
+  .get(`http://${HOSTNAME}:${PORT}/api/${type}?${categoryId ? `categoryId=${categoryId}` : ''}`)
+  .then(response => response.data);
